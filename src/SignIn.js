f13a03ab0369d959220673adb6155324be451cb7
KESHAWN SHARPER <ksharper@studentmba.org>
2024-03-15 17:04:35 -0400

    redeploy

--- src/SignIn.js
+++ src/SignIn.js
@@ -6,7 +6,7 @@ import { Link,useNavigate } from "react-router-dom";
 
 export default function SignIn(props) {
    const [user,setUser] = useState({
-       email:"",
+       email:'',
        password:""
    })
    const [error,setError] = useState({})
